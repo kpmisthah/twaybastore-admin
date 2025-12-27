3ffd6d5bb1e836c2b97598c0bd75beff85601e9f
kpmisthah <kpmisthah6@gmail.com>
2025-12-27 19:05:08 +0530

    fix:localhost sisi

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
 // const BASE_URL = "https://twayba-backend-oln6.onrender.com/api/";
-const BASE_URL = "https://twaybastore-admin.vercel.app"
+const BASE_URL = "https://twaybastore-admin.vercel.app/api"
 export default BASE_URL;

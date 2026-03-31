8dea3d7e30cb46b42e682eb5c2777322b68db435
kpmisthah <kpmisthah6@gmail.com>
2026-03-31 23:28:44 +0530

    fix:Make admin product code optional

--- src/components/AdminAddProduct.jsx
+++ src/components/AdminAddProduct.jsx
@@ -235,9 +235,8 @@ const AdminAddProduct = ({ onDone }) => {
           name="productCode"
           value={form.productCode}
           onChange={handleChange}
-          placeholder="Unique Product Code (for Google Sheets sync) *"
+          placeholder="Unique Product Code (for Google Sheets sync) (optional)"
           className="w-full border px-3 py-2"
-          required
         />
 
         {/* Variants */}
